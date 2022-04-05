7bb59fa58b853829afda12e20e655b9b8207c4b0
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-04-05 23:33:47 +0600

    dynamic revew card

--- src/Component/Review/Card/Card.js
+++ src/Component/Review/Card/Card.js
@@ -9,7 +9,24 @@ function Card(props) {
    
    const {name , logo,star,description}  = props.review ;
   return (
-    
+    <div className="review-card">
+    <div className="header-card">
+        <h5 className="logo">{logo}</h5>
+        <div className="name">
+            <h3>{name}</h3>
+        </div>
+       
+    </div>
+    <Rating
+            name="simple-controlled"
+            value={star}
+            onChange={(event, newValue) => {
+                setValue(newValue);
+            }}
+        />
+
+    <p className="description">"{description.slice(0 , 270)}"</p>
+</div>
   )
 }
 
