ac864e8f057d5994fdacbb571d4b8f34026bbc85
fahimshahriar786 <fahimshahariar786@gmail.com>
2022-04-05 23:32:48 +0600

    navber routers and link

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -4,7 +4,37 @@ import './Navbar.css'
 const Navbar = () => {
 
   return (
-    
+    <nav className="navbar navbar-expand-lg navbar-light bg-danger">
+      <div className="container">
+        <Link className="navbar-brand" to="/"><h2>THE COCA-COLA COMPANY</h2></Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/review">Review</NavLink>
+            </li>
+          
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/dashboard">Dashboard</NavLink>
+            </li>
+          
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/blogs">Blogs</NavLink>
+            </li>
+          
+          
+          </ul>
+       
+        </div>
+      </div>
+    </nav>
+
+
 
   )
 }
